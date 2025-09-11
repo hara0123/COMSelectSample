1adf4a9362a023fcca82459dc5ae2dfa5a097440
Hironori HARA <hara@game.t-kougei.ac.jp>
2025-09-11 19:15:10 +0900

    キー操作追加

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,13 +1,19 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.InputSystem;
 
 public class UIManager : MonoBehaviour
 {
     SerialPortListup serialPortListup_;
+    int selectedIndex_;
+
     public GameObject buttonPrefab_;
     public Transform canvasTransform_;
     public TMP_FontAsset japaneseFont_;
+    public GameObject USBImagePrefab_;
+
+    SignalChangeDetector signalChangeDetector_;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,9 +22,14 @@ public class UIManager : MonoBehaviour
         for (int i = 0; i < serialPortListup_.portNum; i++)
         {
             GameObject buttonObj = Instantiate(buttonPrefab_, canvasTransform_);
-
             // 位置をずらして配置
             buttonObj.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, -100 * i);
+
+            if (i == 0)
+            {
+                USBImagePrefab_ = Instantiate(USBImagePrefab_, canvasTransform_);
+                USBImagePrefab_.GetComponent<RectTransform>().anchoredPosition = buttonObj.GetComponent<RectTransform>().anchoredPosition + new Vector2(-150, 0);
+            }
 
             // ボタンのテキストを変更
             Button buttonComp = buttonObj.GetComponent<Button>();
@@ -42,14 +53,43 @@ public class UIManager : MonoBehaviour
         exitLabel.color = Color.white; // ボタンの文字の色
         exitLabel.font = japaneseFont_; // ボタンの文字のフォント
 
-        int exitIndex = 3;
-        exitButtonComp.onClick.AddListener(() => OnButtonClicked(exitIndex)); // iを直接渡すと全てのボタンに最後の値が渡る。クロージャ問題。
+        exitButtonComp.onClick.AddListener(() => OnButtonClicked(serialPortListup_.portNum));
+
+        selectedIndex_ = 0;
+        signalChangeDetector_ = new SignalChangeDetector(selectedIndex_);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        var current = Keyboard.current;
+
+        if (current.upArrowKey.wasPressedThisFrame)
+            selectedIndex_--;
+        if (current.downArrowKey.wasPressedThisFrame)
+            selectedIndex_++;
+        if (current.enterKey.wasPressedThisFrame)
+        {
+            OnButtonClicked(selectedIndex_);
+        }
+
+        if (selectedIndex_ < 0)
+        {
+            selectedIndex_ = 0;
+        }
+        else if (selectedIndex_ > serialPortListup_.portNum) // exitボタンがあるので条件の書き方はOK
+        {
+            selectedIndex_ = serialPortListup_.portNum;
+        }
+
+        signalChangeDetector_.Input(selectedIndex_);
+
+        if (signalChangeDetector_.IsChanged())
+        {
+            USBImagePrefab_.GetComponent<RectTransform>().anchoredPosition = new Vector2(-150, -100 * selectedIndex_);
+        }
+
+        signalChangeDetector_.Update();
     }
 
     void OnButtonClicked(int buttonIndex)
@@ -63,9 +103,6 @@ public class UIManager : MonoBehaviour
                 Debug.Log("index 1");
                 break;
             case 2:
-                Debug.Log("index 2");
-                break;
-            case 3:
                 // exit
 #if UNITY_EDITOR
                 UnityEditor.EditorApplication.isPlaying = false;
